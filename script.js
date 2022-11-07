098df5600733a5c6e6e72e509a7c839fc3f67b5c
Óscar O <oscarforjuela@hotmail.com>
2022-11-07 00:31:34 -0500

    Comentarios

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-//Declaramos variables
+//Declaramos variables 
+/*SE DECLARAN LAS VARIABLES GLOBALES LAS CUALES SE LES ASIGNA EL VALOR DE LO QUE INGRESA EL USUARIO Y A SU VEZ LA OPERACIÓN A REALIZAR*/
 var operandoa;
 var operandob;
 var operacion;
 
+/*SE CREA LA FUNCIÓN init, LA CUAL ASIGNA TANTO LOS NÚMEROS COMO LAS OPERACIONES A VARIABLES MEDIANTE EL ID (getElementById) ASIGNADO EN EL HTML*/
 function init() {
     //variables
     var resultado = document.getElementById('resultado');
@@ -25,6 +27,8 @@ function init() {
 }
 
 //Eventos de click
+
+/*SE ASIGNA UN EVENTO DE CLIC PARA CADA VARIABLE*/
 uno.onclick = function (e) {
     resultado.textContent = resultado.textContent + "1";
 }
@@ -58,46 +62,51 @@ cero.onclick = function (e) {
 reset.onclick = function (e) {
     resetear();
 }
+/*Se crean las funciones que asignan el valor de un signo + a la variable operación */
 suma.onclick = function (e) {
     operandoa = resultado.textContent;
     operacion = "+";
     limpiar();
 }
+/*Se crean las funciones que asignan el valor de un signo - a la variable operación */
 resta.onclick = function (e) {
     operandoa = resultado.textContent;
     operacion = "-";
     limpiar();
 }
+/*Se crean las funciones que asignan el valor de un signo * a la variable operación */
 multiplicacion.onclick = function (e) {
     operandoa = resultado.textContent;
     operacion = "*";
     limpiar();
 }
+/*Se crean las funciones que asignan el valor de un signo / a la variable operación */
 division.onclick = function (e) {
     operandoa = resultado.textContent;
     operacion = "/";
     limpiar();
 }
+/*Aquí la función nos lleva a otra función que de acuerdo a la operación seleccionada se resolverá con la variables ingresadas anteriormente */
 igual.onclick = function (e) {
     operandob = resultado.textContent;
     resolver();
 }
-
+/*Deja en blanco el campo resultado */
 function limpiar() {
     resultado.textContent = "";
 }
-
+/*Asigna un valor en blanco para las variables Sting y 0 para las variables que reciban números */
 function resetear() {
     resultado.textContent = "";
     operandoa = 0;
     operandob = 0;
     operacion = "";
 }
-
+/*Dependiendo del valor de operación, asignado por el usuario, se realizará la operación de alguna de la opciones en el switch */
 function resolver() {
     var res = 0;
     switch (operacion) {
-        case "+":
+        case "+":/*Se almacena la operación matemática en la variable res */
             res = parseFloat(operandoa) + parseFloat(operandob);
             break;
 
@@ -113,7 +122,7 @@ function resolver() {
             res = parseFloat(operandoa) / parseFloat(operandob);
             break;
     }
-    resetear();
+    resetear();/*Llama a la función resetear para dejar los campos en blanco para el siguiente ingreso de datos */
     
-    resultado.textContent = "El resultado es "+ res;
+    resultado.textContent = "El resultado es "+ res;/*Se muestra la variable res en el elemento con id resultado */
 }
